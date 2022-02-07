1925e6c7c5b5d272445d4c87a3bd6e2b7be0cda7
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-02-07 13:13:41 -0500

    fix(person): Rename is_of_age? to of_age?

--- person.rb
+++ person.rb
@@ -7,7 +7,7 @@ class Person
   end
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
 
   attr_accessor :name, :age
@@ -15,7 +15,7 @@ class Person
 
   private
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 end

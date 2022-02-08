acc09f36f909972e70ab0cdcda4c5b279d8a89fb
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-02-08 13:11:07 -0500

    refactor: Remove unnecessary if statement

--- corrector.rb
+++ corrector.rb
@@ -1,10 +1,5 @@
 class Corrector
   def correct_name(name)
-    name = name.capitalize
-    if name.length > 10
-      name.slice(0, 10)
-    else
-      name
-    end
+    name.capitalize.slice(0, 10)
   end
 end

3657a9230b9431bf0ec959bf6244e359abe6b04a
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-02-08 12:56:42 -0500

    feat: Use Corrector class in Person class

--- person.rb
+++ person.rb
@@ -1,9 +1,16 @@
+require_relative './corrector'
+
 class Person
   def initialize(age, name = 'Unknow', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new
+  end
+
+  def validate_name
+    @name = @corrector.correct_name(@name)
   end
 
   def can_use_services?

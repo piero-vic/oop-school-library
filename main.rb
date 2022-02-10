667150e42f5213a9c444d00bc3c8ee19669ec69b
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-02-10 11:28:44 -0500

    feat: Add list people functionality

--- main.rb
+++ main.rb
@@ -28,6 +28,11 @@ def one_line_prompt(message)
   gets.chomp
 end
 
+# 2 - List all people
+def list_people
+  puts @people.map {|p| "[#{p.class.name}] Name:#{p.name}, ID:#{p.id}, Age:#{p.age} " }
+end
+
 # 3 - Create a person
 def create_person
   selection = one_line_prompt('Do you want to create a student (1) or a teacher (2)? [input the number]: ').to_i
@@ -52,24 +57,26 @@ def create_person
 end
 
 def main
-  x = initial_prompt
-  case x
-  when 1
-    puts '1 - List all books'
-  when 2
-    puts '2 - List all people'
-  when 3
-    create_person
-  when 4
-    puts '4 - Create a book'
-  when 5
-    puts '5 - Create a rental'
-  when 6
-    puts '6 - List all rentals for a given person'
-  when 7
-    exit
-  else
-    puts "ERROR: #{x} - Invalid input"
+  loop do
+    x = initial_prompt
+    case x
+    when 1
+      puts '1 - List all books'
+    when 2
+      list_people
+    when 3
+      create_person
+    when 4
+      puts '4 - Create a book'
+    when 5
+      puts '5 - Create a rental'
+    when 6
+      puts '6 - List all rentals for a given person'
+    when 7
+      break
+    else
+      puts "ERROR: #{x} - Invalid input"
+    end
   end
 end
 
